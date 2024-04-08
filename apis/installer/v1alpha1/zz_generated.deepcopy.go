20be889843e2290c7a4a19bd1cfa2eeb850e154e
Tamal Saha <tamal@appscode.com>
2024-04-08 01:37:34 -0700

    Separate commands for operator and webhook server
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/zz_generated.deepcopy.go
+++ apis/installer/v1alpha1/zz_generated.deepcopy.go
@@ -46,6 +46,25 @@ func (in *ApparmorSpec) DeepCopy() *ApparmorSpec {
 func (in *CleanerRef) DeepCopyInto(out *CleanerRef) {
 	*out = *in
 	in.ImageRef.DeepCopyInto(&out.ImageRef)
+	if in.NodeSelector != nil {
+		in, out := &in.NodeSelector, &out.NodeSelector
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
+	if in.Tolerations != nil {
+		in, out := &in.Tolerations, &out.Tolerations
+		*out = make([]v1.Toleration, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.Affinity != nil {
+		in, out := &in.Affinity, &out.Affinity
+		*out = new(v1.Affinity)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
