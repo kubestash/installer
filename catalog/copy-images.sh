d94a61343802da9ed4f7cac5060d7b0c5f97f817
lgtm <1gtm@users.noreply.github.com>
2026-02-26 03:02:36 +0530

    Prepare for release v2026.2.27 (#297)
    ProductLine: KubeStash
    
    Release: v2026.2.27
    
    Release-tracker: https://github.com/kubestash/CHANGELOG/pull/41
    
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/copy-images.sh
+++ catalog/copy-images.sh
@@ -36,9 +36,9 @@ mv /tmp/crane .
 CMD="./crane"
 
 $CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/appscode/kubectl-nonroot:1.34 $IMAGE_REGISTRY/appscode/kubectl-nonroot:1.34
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/kubedump:v0.23.0-rc.0 $IMAGE_REGISTRY/kubestash/kubedump:v0.23.0-rc.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/kubestash:v0.24.0-rc.0 $IMAGE_REGISTRY/kubestash/kubestash:v0.24.0-rc.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/manifest:v0.16.0-rc.0 $IMAGE_REGISTRY/kubestash/manifest:v0.16.0-rc.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/pvc:v0.23.0-rc.0 $IMAGE_REGISTRY/kubestash/pvc:v0.23.0-rc.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/volume-snapshotter:v0.23.0-rc.0 $IMAGE_REGISTRY/kubestash/volume-snapshotter:v0.23.0-rc.0
-$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/workload:v0.23.0-rc.0 $IMAGE_REGISTRY/kubestash/workload:v0.23.0-rc.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/kubedump:v0.23.0 $IMAGE_REGISTRY/kubestash/kubedump:v0.23.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/kubestash:v0.24.0 $IMAGE_REGISTRY/kubestash/kubestash:v0.24.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/manifest:v0.16.0 $IMAGE_REGISTRY/kubestash/manifest:v0.16.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/pvc:v0.23.0 $IMAGE_REGISTRY/kubestash/pvc:v0.23.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/volume-snapshotter:v0.23.0 $IMAGE_REGISTRY/kubestash/volume-snapshotter:v0.23.0
+$CMD cp --allow-nondistributable-artifacts --insecure ghcr.io/kubestash/workload:v0.23.0 $IMAGE_REGISTRY/kubestash/workload:v0.23.0
