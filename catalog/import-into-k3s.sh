e84b95004b6a084c96779c854b13007f44b0cc87
1gtm <1gtm@appscode.com>
2026-02-25 19:20:21 +0000

    Prepare for release v2026.2.27
    ProductLine: KubeStash
    
    Release: v2026.2.27
    
    Release-tracker: https://github.com/kubestash/CHANGELOG/pull/41
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/import-into-k3s.sh
+++ catalog/import-into-k3s.sh
@@ -25,9 +25,9 @@ TARBALL=${1:-}
 tar -zxvf $TARBALL
 
 k3s ctr images import images/appscode-kubectl-nonroot-1.34.tar
-k3s ctr images import images/kubestash-kubedump-v0.23.0-rc.0.tar
-k3s ctr images import images/kubestash-kubestash-v0.24.0-rc.0.tar
-k3s ctr images import images/kubestash-manifest-v0.16.0-rc.0.tar
-k3s ctr images import images/kubestash-pvc-v0.23.0-rc.0.tar
-k3s ctr images import images/kubestash-volume-snapshotter-v0.23.0-rc.0.tar
-k3s ctr images import images/kubestash-workload-v0.23.0-rc.0.tar
+k3s ctr images import images/kubestash-kubedump-v0.23.0.tar
+k3s ctr images import images/kubestash-kubestash-v0.24.0.tar
+k3s ctr images import images/kubestash-manifest-v0.16.0.tar
+k3s ctr images import images/kubestash-pvc-v0.23.0.tar
+k3s ctr images import images/kubestash-volume-snapshotter-v0.23.0.tar
+k3s ctr images import images/kubestash-workload-v0.23.0.tar
