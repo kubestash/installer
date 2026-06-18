79c3cccf53eff9eab5ec135b280d686c8e8e4900
1gtm <1gtm@appscode.com>
2026-06-18 04:52:08 +0000

    Prepare for release v2026.6.18-rc.2
    ProductLine: KubeStash
    
    Release: v2026.6.18-rc.2
    
    Release-tracker: https://github.com/kubestash/CHANGELOG/pull/51
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- catalog/import-images.sh
+++ catalog/import-images.sh
@@ -27,10 +27,10 @@ tar -zxvf $TARBALL
 CMD="./crane"
 
 $CMD push --allow-nondistributable-artifacts --insecure images/appscode-kubectl-nonroot-1.34.tar $IMAGE_REGISTRY/appscode/kubectl-nonroot:1.34
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-kubedump-v0.27.0-rc.0.tar $IMAGE_REGISTRY/kubestash/kubedump:v0.27.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-kubestash-v0.28.0-rc.0.tar $IMAGE_REGISTRY/kubestash/kubestash:v0.28.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-manifest-v0.20.0-rc.0.tar $IMAGE_REGISTRY/kubestash/manifest:v0.20.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-pvc-v0.27.0-rc.0.tar $IMAGE_REGISTRY/kubestash/pvc:v0.27.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-vault-v0.2.0-rc.0.tar $IMAGE_REGISTRY/kubestash/vault:v0.2.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-volume-snapshotter-v0.27.0-rc.0.tar $IMAGE_REGISTRY/kubestash/volume-snapshotter:v0.27.0-rc.0
-$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-workload-v0.27.0-rc.0.tar $IMAGE_REGISTRY/kubestash/workload:v0.27.0-rc.0
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-kubedump-v0.27.0-rc.2.tar $IMAGE_REGISTRY/kubestash/kubedump:v0.27.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-kubestash-v0.28.0-rc.2.tar $IMAGE_REGISTRY/kubestash/kubestash:v0.28.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-manifest-v0.20.0-rc.2.tar $IMAGE_REGISTRY/kubestash/manifest:v0.20.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-pvc-v0.27.0-rc.2.tar $IMAGE_REGISTRY/kubestash/pvc:v0.27.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-vault-v0.2.0-rc.2.tar $IMAGE_REGISTRY/kubestash/vault:v0.2.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-volume-snapshotter-v0.27.0-rc.2.tar $IMAGE_REGISTRY/kubestash/volume-snapshotter:v0.27.0-rc.2
+$CMD push --allow-nondistributable-artifacts --insecure images/kubestash-workload-v0.27.0-rc.2.tar $IMAGE_REGISTRY/kubestash/workload:v0.27.0-rc.2
